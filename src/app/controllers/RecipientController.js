2a40c7de0cf85a1d59985635baf368babdb5f017
vargattj <vargattjh@gmail.com>
2020-07-04 21:35:29 -0300

    recipients table

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,63 @@
+import * as Yup from "yup";
+import Recipient from "../models/Recipient";
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation fails" });
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip_code,
+    } = await Recipient.create(req.body);
+
+    res.json({ id, name, street, number, complement, state, city, zip_code });
+  }
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zip_code: Yup.string(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation fails" });
+    }
+
+    const { id } = req.params;
+    const recipient = await Recipient.findByPk(id);
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip_code,
+    } = await recipient.update(req.body);
+
+    res.json({ id, name, street, number, complement, state, city, zip_code });
+  }
+}
+
+export default new RecipientController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "aae8914be3e00db45473373fccb96cj9",
+  expiresIn: "7d",
+};
